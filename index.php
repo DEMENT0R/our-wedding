4646e5f6964ea800f66938970462a68f335c9f19
Maxim Bezrukavnikov <dmntr@rambler.ru>
2021-11-18 18:53:06 +0300

    Update index.php

--- index.php
+++ index.php
@@ -110,34 +110,15 @@
     </script>
     <script>
         document.addEventListener("DOMContentLoaded", function() {
-          var lazyloadImages;    
+          var lazyloadImages = document.querySelectorAll("img.lazy");    
+          var lazyloadThrottleTimeout;
 
-          if ("IntersectionObserver" in window) {
-            lazyloadImages = document.querySelectorAll(".lazy");
-            var imageObserver = new IntersectionObserver(function(entries, observer) {
-              entries.forEach(function(entry) {
-                if (entry.isIntersecting) {
-                  var image = entry.target;
-                  image.src = image.dataset.src;
-                  image.classList.remove("lazy");
-                  imageObserver.unobserve(image);
-                }
-              });
-            });
+          function lazyload () {
+            if(lazyloadThrottleTimeout) {
+              clearTimeout(lazyloadThrottleTimeout);
+            }    
 
-            lazyloadImages.forEach(function(image) {
-              imageObserver.observe(image);
-            });
-          } else {  
-            var lazyloadThrottleTimeout;
-            lazyloadImages = document.querySelectorAll(".lazy");
-
-            function lazyload () {
-              if(lazyloadThrottleTimeout) {
-                clearTimeout(lazyloadThrottleTimeout);
-              }    
-
-              lazyloadThrottleTimeout = setTimeout(function() {
+            lazyloadThrottleTimeout = setTimeout(function() {
                 var scrollTop = window.pageYOffset;
                 lazyloadImages.forEach(function(img) {
                     if(img.offsetTop < (window.innerHeight + scrollTop)) {
@@ -150,14 +131,13 @@
                   window.removeEventListener("resize", lazyload);
                   window.removeEventListener("orientationChange", lazyload);
                 }
-              }, 20);
-            }
-
-            document.addEventListener("scroll", lazyload);
-            window.addEventListener("resize", lazyload);
-            window.addEventListener("orientationChange", lazyload);
+            }, 20);
           }
-        })
+
+          document.addEventListener("scroll", lazyload);
+          window.addEventListener("resize", lazyload);
+          window.addEventListener("orientationChange", lazyload);
+        });
         </script>
 </body>
 </html>
